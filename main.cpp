4fb49da0c62cb1a8d7276eab41c8350b139dba2b
lil64 <yaroslavburak1337@gmail.com>
2022-06-03 16:04:33 +0300

    first commit

---
+++ main.cpp
@@ -0,0 +1,45 @@
+#include <sys\stat.h>
+#include <iostream>
+#include <dirent.h>
+#include <conio.h>
+
+using namespace std;
+
+int main ()
+{
+	struct dirent *d;
+	struct stat dst;
+	
+	DIR *dr;
+	
+	string path = ".\\";
+	
+	dr = opendir(path.c_str());
+	
+	if (dr != NULL)
+	{
+		for (d = readdir(dr); d != NULL; d = readdir(dr))
+		{
+			string type = d->d_name;
+			type = path + type;
+			if (stat(type.c_str(), &dst) == 0)
+			{
+				if (dst.st_mode & S_IFDIR)
+				{
+					type = "is a FOLDER.";
+				}
+				else if (dst.st_mode & S_IFREG)
+				{
+					type = "is a FILE.";
+				}
+			}
+			std::cout <<d->d_name<< std::endl <<type<< std::endl;
+		}
+		closedir(dr);
+	}
+	else
+	{
+		std::cout <<"ERROR"<< std::endl;
+	}
+	getch();
+}
